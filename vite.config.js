9b3d7d0c3c4a6a07caff9f9aebcfc4e37b20067f
carlnoval <cjrnoval22@gmail.com>
2023-06-22 02:35:31 +0900

    Update: The alias option has been replaced by resolve.alias

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// manually added
+// manually added import
 import path from 'path'
 
-// original from vite
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-// })
 
 // modified to to be able to use the @ symbol as an alias for the source directory, similar to Vue CLI created projects
 export default defineConfig({
-  alias: {
-    '@' : path.resolve(__dirname, './src')
-  },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@' : path.resolve(__dirname, './src')
+    },
+  },
 })
