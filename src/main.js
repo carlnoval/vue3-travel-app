ba30ec9ef69c423c7845267103fe81f9dbb2235b
carlnoval <cjrnoval22@gmail.com>
2023-06-22 02:19:38 +0900

    Add: Route setup

--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// manually added import
+import {createRouter, createWebHashHistory} from 'vue-router'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+
+  ]
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
