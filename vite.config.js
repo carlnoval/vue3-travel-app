6fba76761dc5f0315670f8f20b3d5c9fc0940c6d
carlnoval <cjrnoval22@gmail.com>
2023-06-22 01:51:21 +0900

    Update: Setup to allow use of @ symbol for source directory, similar to Vue CLI created projects

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// manually added
+import path from 'path'
+
+// original from vite
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
+// modified to to be able to use the @ symbol as an alias for the source directory, similar to Vue CLI created projects
 export default defineConfig({
+  alias: {
+    '@' : path.resolve(__dirname, './src')
+  },
   plugins: [vue()],
 })
